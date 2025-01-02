67e9cb4675100edade484c7554e680083bd917a6
13man <94jws@naver.com>
2025-01-02 14:13:46 +0900

    main update

---
+++ frontend/src/html/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './css/main.css';
+import Logo from '../resource/Logo.png';
+import MainBack from '../resource/MAIN-BACK.png';
+import GuideIcon from '../resource/guide.png';
+import CameraIcon from '../resource/Cam.png';
+import AlbumIcon from '../resource/elbum.png';
+
+function MainPage() {
+    return (
+        <div className="page">
+            <div className="main">
+                <div className="language-selector">
+                    <select id="language">
+                        <option value="en">English</option>
+                        <option value="ch">中文</option>
+                        <option value="ja">日本語</option>
+                    </select>
+                </div>
+                <div className="background-container">
+                    <img src={Logo} alt="logo" className="Logo" />
+                    <img src={MainBack} alt="background" className="background" />
+                </div>
+            </div>
+
+            <div className="underBar">
+                <img src={GuideIcon} alt="guide" className="under-item" />
+                <img src={CameraIcon} alt="Camera" className="under-item" />
+                <img src={AlbumIcon} alt="album" className="under-item" />
+            </div>
+        </div>
+    );
+}
+export default MainPage; 
